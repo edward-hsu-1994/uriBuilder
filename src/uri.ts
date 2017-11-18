898cf012f83f40405fa621a8ff1166c9a72f3b92
DESKTOP-NKNK6UG\XuPeiYao <st89382000@gmail.com>
2017-11-19 00:28:10 +0800

    Implement URI schema, host and port parse

--- src/uri.ts
+++ src/uri.ts
@@ -3,8 +3,8 @@ import { UriSchemaPortList } from './uriSchemaPortList';
 
 // #region Uri Format Regex
 const uriRegExp = /^(([^:/?#]+):)\/\/([^/?#]+)(\?([^#]*))?(#(.*))?/;
-const uriRegExp_Schema = /^(([^:/?#]+):)/;
-const uriRegExp_HostPort = /\/\/[^/?#]+/;
+const uriRegExp_schema = /^(([^:/?#]+):)/;
+const uriRegExp_hostPort = /\/\/[^/?#]+/;
 const uriRegExp_pathSegments = /(\/[^/?#]*)+/;
 const uriRegExp_quert = /\?[^#]*/;
 const uriRegExp_hash = /#(.*)/;
@@ -31,5 +31,23 @@ export class Uri {
 
   public static isUriFormat(str: string): boolean {
     return uriRegExp.test(str);
+  }
+
+  public static parse(uri: string): Uri {
+    if (!this.isUriFormat(uri)) {
+      throw new Error('URI Format ERROR');
+    }
+
+    const result = new Uri();
+
+    result.schema = uri.match(uriRegExp_schema)[0];
+    result.schema = result.schema.substring(0, result.schema.length - 1);
+
+    result.host = uri.match(uriRegExp_hostPort)[0].substring(2);
+    const hostPortTemp = result.host.split(':', 2);
+    result.host = hostPortTemp[0];
+    result.port = +hostPortTemp[1];
+
+    return result;
   }
 }
